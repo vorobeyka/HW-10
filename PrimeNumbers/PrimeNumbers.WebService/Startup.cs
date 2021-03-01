44feaa14c6b925a474425827e793c0476d40a07a
vorobeyka <vorobeyka3222@gmail.com>
2021-03-01 17:51:59 +0200

    some fix

--- PrimeNumbers/PrimeNumbers.WebService/Startup.cs
+++ PrimeNumbers/PrimeNumbers.WebService/Startup.cs
@@ -33,14 +33,7 @@ namespace PrimeNumbers.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
